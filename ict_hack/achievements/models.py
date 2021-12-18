08416a352c587e71359a16d89eb55e395f024a11
Georgy Komarov <georgy.komarov@mail.ru>
2021-12-19 00:03:19 +0300

    Add meta to Achievement

--- ict_hack/achievements/models.py
+++ ict_hack/achievements/models.py
@@ -7,6 +7,10 @@ from utils.models import ExtendedModel
 
 
 class Achievement(ExtendedModel):
+    class Meta:
+        verbose_name = 'Достижение'
+        verbose_name_plural = 'Достижения'
+
     event = models.ForeignKey(Event, on_delete=models.PROTECT, null=True, related_name='achievements', verbose_name='Мероприятие')
     name = models.CharField(max_length=100, verbose_name='Название')
     score = models.PositiveSmallIntegerField(validators=[MinValueValidator(1)], verbose_name='Баллы')
