8e1a521df9c1d53ff25b8d2ccc95442bfe929817
Georgy Komarov <georgy.komarov@mail.ru>
2021-12-19 08:47:05 +0300

    Add TrophyAdmin

--- ict_hack/trophies/admin.py
+++ ict_hack/trophies/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from trophies.models import Trophy
+from utils.admin import ExtendedAdmin
+
+
+@admin.register(Trophy)
+class TrophyAdmin(ExtendedAdmin):
+    pass
